490a34338196fc3d44b5b5b4e574f0859e644cdc
StevanStojsin <36681925+StevanStojsin@users.noreply.github.com>
2018-03-27 12:46:16 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var certificate = document.getElementsByClassName("certificate");
+
+    // for (var i = 0; i < certificate.length; i++){
+    //     $(certificate[i]).click(function(){
+    //         $(certificate).css("display", "none");
+    //         $(this).css({
+    //             "display": "block",
+    //             "width": "60%"
+    //         });
+    //     });
+
+        // ovako je javaScript, iznad je jquery
+        // certificate[i].addEventListener("click", function(){
+        //     alert("klik");
+        // });
+    // }
+
+});
